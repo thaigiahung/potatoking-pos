82e63da446b1a485fa27590e80152ff89b01c4d6
Gia Hung Thai <hung.tg89@gmail.com>
2015-10-10 09:43:08 +0700

    Add model Device

---
+++ api/models/Device.js
@@ -0,0 +1,19 @@
+/**
+* Device.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+    ip: { type: 'string' },
+    mac: { type: 'string' },
+    status: {
+      type: 'string',
+      enum: ['enable', 'disable']
+    },
+  }
+};
+
